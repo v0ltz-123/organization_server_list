58d81d1384b03748a237e380b3f839561e26c50c
v0ltz-123 <ljs4las@gmail.com>
2025-04-23 22:38:20 -0400

    Fix: use Railway env vars for PostgreSQL connection

--- app.py
+++ app.py
@@ -13,15 +13,17 @@ app.secret_key = 'fovd#BD^2OYvj!a2NUnAvvWAjwNL%$KRuNcrxL8uave4v0YePf$JlMbBWO#I&b
 ADMIN_PASSWORD_HASH = 'scrypt:32768:8:1$Tal9Kqn3VUUMB7dn$74d887280c2bc69582109450391bf89f6799929f9aa224c166d7a99221d39021c8294c145c5486e24d5d2d234762ded96d2f0eafe3c819b98f17953dc2d13964'
 
 
-# Connect to your PostgreSQL database
+# Connect to your PostgreSQL database on Railway
 def get_db_connection():
     conn = psycopg2.connect(
-        host=os.getenv('DB_HOST'),
-        database=os.getenv('DB_NAME'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD')
+        host=os.getenv('PGHOST'),
+        database=os.getenv('PGDATABASE'),
+        user=os.getenv('PGUSER'),
+        password=os.getenv('PGPASSWORD'),
+        port=os.getenv('PGPORT')
     )
     return conn
+
 
 # --- HOME PAGE ROUTE ---
 @app.route('/', methods=['GET', 'POST'])
