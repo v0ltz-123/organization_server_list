7c9ac10798e6db78d08f7d1f2e18ea915fce423c
v0ltz-123 <ljs4las@gmail.com>
2025-04-25 20:48:26 -0400

    Initial commit

--- app.py
+++ app.py
@@ -13,16 +13,19 @@ app.secret_key = 'fovd#BD^2OYvj!a2NUnAvvWAjwNL%$KRuNcrxL8uave4v0YePf$JlMbBWO#I&b
 ADMIN_PASSWORD_HASH = 'scrypt:32768:8:1$Tal9Kqn3VUUMB7dn$74d887280c2bc69582109450391bf89f6799929f9aa224c166d7a99221d39021c8294c145c5486e24d5d2d234762ded96d2f0eafe3c819b98f17953dc2d13964'
 
 
-# Connect to your PostgreSQL database on Railway
+# Load environment variables from .env
+
+
 def get_db_connection():
     conn = psycopg2.connect(
-        host=os.getenv('PGHOST'),
-        database=os.getenv('PGDATABASE'),
-        user=os.getenv('PGUSER'),
-        password=os.getenv('PGPASSWORD'),
-        port=os.getenv('PGPORT')
+        host=os.environ['DB_HOST'],
+        database=os.environ['DB_NAME'],
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASSWORD'],
+        port=os.environ.get('DB_PORT', 5432)  # use 5432 if not specified
     )
     return conn
+
 
 
 # --- HOME PAGE ROUTE ---
